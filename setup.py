2a30da371d8994bc19bb8b8acf297a2886a826e7
kositzyn <kositzyn@gmail.com>
2023-09-22 14:24:44 +0400

    Add Dockerfile

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import requests
 import json
 
-url = 'http://127.0.0.1:8000/auth/register/'
+from sqlalchemy import create_engine
 
-data = {"username": "root",
-        "email": "root",
-        "csvfile": "demo",
-        "password": "root",
-        "is_active": False,
-        "is_superuser": True,
-        "is_verified": False}
+from app.database import DATABASE_URL
 
-response = requests.post(url, data=json.dumps(data))
-if response.status_code == 201:
-        print('User root has been created successfully')
+# url = 'http://localhost:8000/auth/register/'
+#
+# data = {"username": "root",
+#         "email": "root",
+#         "csvfile": "demo",
+#         "password": "root",
+#         "is_active": False,
+#         "is_superuser": True,
+#         "is_verified": False}
+#
+# response = requests.post(url, data=json.dumps(data))
+# if response.status_code == 201:
+#         print('User root has been created successfully')
+
 
